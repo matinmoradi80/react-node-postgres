8063ac0769ce5f2a546fcec29fd922d2e5ae0548
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2022-02-01 00:41:33 +0330

    connecting to postgress

--- react-postgres/src/App.js
+++ react-postgres/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [merchants, setMerchants] = useState(false);
@@ -8,45 +8,45 @@ function App() {
   }, []);
 
   function getMerchant() {
-    fetch('http://localhost:3001')
-      .then(response => {
+    fetch("http://localhost:3001")
+      .then((response) => {
         return response.text();
       })
-      .then(data => {
+      .then((data) => {
         setMerchants(data);
       });
   }
 
   function createMerchant() {
-    let name = prompt('Enter merchant name');
-    let email = prompt('Enter merchant email');
+    let name = prompt("Enter merchant name");
+    let email = prompt("Enter merchant email");
 
-    fetch('http://localhost:3001/merchants', {
-      method: 'POST',
+    fetch("http://localhost:3001/merchants", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({name, email}),
+      body: JSON.stringify({ name, email }),
     })
-      .then(response => {
+      .then((response) => {
         return response.text();
       })
-      .then(data => {
+      .then((data) => {
         alert(data);
         getMerchant();
       });
   }
 
   function deleteMerchant() {
-    let id = prompt('Enter merchant id');
+    let id = prompt("Enter merchant id");
 
     fetch(`http://localhost:3001/merchants/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     })
-      .then(response => {
+      .then((response) => {
         return response.text();
       })
-      .then(data => {
+      .then((data) => {
         alert(data);
         getMerchant();
       });
@@ -54,7 +54,7 @@ function App() {
 
   return (
     <div>
-      {merchants ? merchants : 'There is no merchant data available'}
+      {merchants ? merchants : "There is no merchant data available"}
       <br />
       <button onClick={createMerchant}>Add</button>
       <br />
