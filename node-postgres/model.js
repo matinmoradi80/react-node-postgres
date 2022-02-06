525e7db3105730ba80fb99b0e60f0dacec0b212c
mmm1379 <mmmasoudpour@gmail.com>
2022-02-06 13:18:45 +0330

    some changes in model.js

--- node-postgres/model.js
+++ node-postgres/model.js
@@ -289,6 +289,294 @@ const getManager = () => {
   });
 };
 
+
+const getProductCategory = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM productCategory", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getProduct = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM product", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getComment = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM comment", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getLoad = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM load", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getLoadProduct = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM loadProduct", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getPurchase = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM purchase", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getDiscount = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM discount", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getDeliveryManUser = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM deliveryManUser", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+const getProductComment = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM productComment", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const getUserOrder = () => {
+    return new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM userOrder", (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.rows);
+            }
+        });
+    });
+};
+
+
+const createProductCategory = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { name, photo } = body;
+        pool.query(
+            "Insert into productCategory (name, photo) values ($1, $2)",
+            [name, photo],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new category has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+const createProduct = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { productId, name, price, qty, photo1, photo2, photo3, photo4, photo5, manufactureDate, expirationDate, categoryName } = body;
+        pool.query(
+            "Insert into product (productId, name, price, qty, photo1, photo2, photo3, photo4, photo5, manufactureDate, expirationDate, categoryName) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+            [productId, name, price, qty, photo1, photo2, photo3, photo4, photo5, manufactureDate, expirationDate, categoryName],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new product has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+const createComment = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { commentId, text, time, productId, nationalCode} = body;
+        pool.query(
+            "Insert into product (commentId, text, time, productId, nationalCode) values ($1, $2, $3, $4, $5)",
+            [commentId, text, time, productId, nationalCode],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new comment has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+const createLoad = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { loadId, date, nationalCode } = body;
+        pool.query(
+            "Insert into product (loadId, date, nationalCode) values ($1, $2, $3)",
+            [loadId, date, nationalCode],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new load has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+const createLoadProduct = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { productId, LoadId } = body;
+        pool.query(
+            "Insert into product (productId, LoadId) values ($1, $2)",
+            [productId, LoadId],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new loadProduct has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+
+const createPurchase = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { nationalCode, orderId, productid ,productQTY } = body;
+        pool.query(
+            "Insert into product (nationalCode, orderId, productid ,productQTY) values ($1, $2, $3, $4)",
+            [nationalCode, orderId, productid ,productQTY],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new purchase has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+
+const createDiscount = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { discountId,nationalCode, amount, max, endDate } = body;
+        pool.query(
+            "Insert into product (discountId,nationalCode, amount, max, endDate) values ($1, $2, $3, $4, $5)",
+            [discountId,nationalCode, amount, max, endDate],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                    console.log(error);
+                } else {
+                    resolve(
+                        `A new discount has been added: ${JSON.stringify(
+                            results.rows[0]
+                        )}`
+                    );
+                }
+            }
+        );
+    });
+};
+
+
+
 module.exports = {
   getUsers,
   createUser,
